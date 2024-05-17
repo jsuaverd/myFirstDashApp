70f7187e41015b57227965b204e65858d78cdfd8
jsuaverd <jsuaverd@my.centennialcollege.ca>
2024-05-17 14:14:40 -0400

    with data, Graph, and Controls

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dash_table, dcc
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 
@@ -6,9 +6,21 @@ import plotly.express as px
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 app = Dash()
 
-app.layout = [html.Div(children = 'My First App with Data'),
+app.layout = [html.Div(children = 'My First App with Data, Graph, and Controls'),
+              html.Hr(),
+              dcc.RadioItems(options = ['pop', 'lifeExp', 'gdpPercap'], value = 'pop', id = 'controls-and-radio-item'),
               dash_table.DataTable(data = df.to_dict('records'), page_size = 10),
-              dcc.Graph(figure = px.histogram(df, x = 'continent', y = 'lifeExp', histfunc = 'avg'))]
+              dcc.Graph(figure = {}, id = 'controls-and-graph')]
+
+@callback(
+    Output(component_id = 'controls-and-graph', component_property = 'figure'),
+    Input(component_id = 'controls-and-radio-item', component_property = 'value')
+)
+
+def update_graph(col_chosen):
+    fig = px.histogram(df, x = 'continent', y=col_chosen, histfunc = 'avg')
+    return fig
+
 
 if __name__ == '__main__':
     app.run(debug = True)
